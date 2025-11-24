c64db73661dd84fa96558e32c4d0bc7661c5fd76
raseuki06 <muhammadraseuki864@gmail.com>
2025-11-24 23:01:57 +0700

    Update Pengguna.java

--- bin/src/model/Pengguna.java
+++ bin/src/model/Pengguna.java
@@ -8,3 +8,19 @@ public abstract class Pengguna {
     private String nama;
     private String username;
     private String password;
+
+    public Pengguna(String idPengguna, String nama, String username, String password) {
+        this.idPengguna = idPengguna;
+        this.nama = nama;
+        this.username = username;
+        this.password = password;
+    }
+
+    // Abstraction & Polymorphism
+    public abstract void tampilkanMenu(); 
+
+    // Encapsulation: Getters
+    public String getIdPengguna() {
+        return idPengguna;
+    }
+
