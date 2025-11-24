88c19d74d702f3787102400bb33143aeb2b03a9b
KhalishaUfairah <khalishaufairah@gmail.com>
2025-11-24 16:53:39 +0700

    Anggota.java

--- bin/src/model/Anggota.java
+++ bin/src/model/Anggota.java
@@ -14,3 +14,19 @@ public class Anggota extends Pengguna {
     /**
      * Implementasi method abstrak: menampilkan menu Anggota.
      */
+     @Override
+    public void tampilkanMenu() {
+        System.out.println("\n=== MENU ANGGOTA ===");
+        System.out.println("1. Lihat Daftar Buku");
+        System.out.println("2. Pinjam Buku");
+        System.out.println("3. Kembalikan Buku");
+        System.out.println("4. Lihat Riwayat Peminjaman Saya");
+        System.out.println("0. Logout");
+        System.out.print("Pilih menu: ");
+    }
+
+    // Encapsulation: Getter
+    public String getTanggalBergabung() {
+        return tanggalBergabung;
+    }
+}
